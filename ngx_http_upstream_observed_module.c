6279a2b7471b897425792f5a2912a53309cee403
robo <hop2deep@gmail.com>
2013-06-21 23:10:38 +0530

    rr and some generic func definitions

--- ngx_http_upstream_observed_module.c
+++ ngx_http_upstream_observed_module.c
@@ -141,3 +141,345 @@ static ngx_uint_t ngx_http_upstream_observed_shm_size;
 static ngx_shm_zone_t *ngx_http_upstream_observed_shm_zone
 static ngx_rbtree_t *ngx_http_upstream_observed_rbtree;
 static ngx_uint_t ngx_http_upstream_observed_generation; 
+
+static int ngx_http_upstream_observed_compare_rbtree_node(const ngx_rbtree_node_t *v_left, const ngx_rbtree_node_t *v_right) {
+  ngx_http_upstream_observed_shm_block_t *left, *right;
+
+  left = (ngx_http_upstream_observed_shm_block_t *) v_left;
+  right = (ngx_http_upstream_observed_shm_block_t *) v_right;
+
+  if(left->generation < right->generation) {
+    return -1;
+  }else if(left->generation > right->generation){
+    return 1;
+  }else {
+    if(left->peers < right->peers){
+      return -1;
+    }else if(left->peers > right>peers) {
+      return 1;
+    }else {
+      return 0;
+    }
+  }
+}
+
+static void ngx_rbtree_generic_insert(ngx_rbtree_node_t *temp, ngx_rbtree_node_t *node, ngx_rbtree_node_t *sentinel, int (*compare)(const ngx_rbtree_node_t *left, const ngx_rbtree_node_t *right)) {
+  for(;;) {
+    if(node->key < temp->key) {
+      if(temp->left == sentinel) {
+	temp->left = node;
+	break;
+      }
+      temp = temp->left;
+    }else if(node->key > temp->key) {
+      if(temp->right == sentinel) {
+	temp->right = node;
+	break;
+      }
+      temp = temp->right;
+    }else {
+      if(compare(node,temp) < 0) {
+	if(temp->left == sentinel) {
+	  temp->left = node;
+	  break;
+	}
+	temp = temp->left;
+      }else {
+	if(temp->right == sentinel) {
+	  temp->right = node;
+	  break;
+	}
+	temp = temp->right;
+      }
+    }
+  }
+  node->parent = temp;
+  node->left = sentinel;
+  node->right = sentinel;
+  ngx_rbt_red(node);
+}
+
+#define NGX_BIVECTOR_ELT_SIZE (sizeof(uintptr_t) * 8)
+
+static uintptr_t *ngx_bivector_alloc(ngx_pool_t *pool, ngx_uint_t size, uintptr_t *small) {
+  ngx_uint_t nelts = (size + NGX_BIVECTOR_ELT_SIZE - 1) / NGX_BIVECTOR_ELT_SIZE;
+
+  if(small && nelts == 1) {
+    *small = 0;
+    return small;
+  }
+
+  return ngx_pcalloc(pool, nelts *NGX_BIVECTOR_ELT_SIZE);
+}
+
+static ngx_int_t ngx_bivector_test(uintptr_t *bv, ngx_uint_t bit) {
+  ngx_uint_t n, m;
+
+  n = bit / NGX_BIVECTOR_ELT_SIZE;
+  m = 1 << (bit % NGX_BIVECTOR_ELT_SIZE);
+
+  return bv[n] & m;
+}
+
+static void ngx_bivector_set(uintptr_t *bv, ngx_uint_t bit) {
+  ngx_uint_t n, m;
+
+  n = bit / NGX_BIVECTOR_ELT_SIZE:
+  m = 1 << (bit % NGX_BIVECTOR_ELT_SIZE);
+
+  bv[n] |= m;
+}
+
+static ngx_int_t ngx_http_upstream_init_module(ngx_cycle_t *cycle) {
+  ngx_http_upstream_observed_generation++;
+  return NGX_OK;
+}
+
+static void ngx_http_upstream_observed_rbtree_insert(ngx_rbtree_node_t *temp, ngx_rbtree_node_t *node, ngx_rbtree_node_t *sentinel) {
+  ngx_rbtree_generic_insert(temp, node, sentinel, ngx_http_upstream_observed_compare_rbtree_node);
+}
+
+static ngx_int_t ngx_http_upstream_observed_init_shm_zone(ngx_shm_zone_t *shm_zone, void *data) {
+  ngx_slab_pool_t   *shpool;
+  ngx_rbtree_t      *tree;
+  ngx_rbtree_node_t *sentinel;
+
+  if(data) {
+    shm_zone->data = data;
+    return NGX_OK;
+  }
+
+  shpool = (ngx_slab_pool_t *) shm_zone->shm.addr;
+  tree = ngx_slab_alloc(shpool, sizeof *tree);
+  if(tree == NULL) {
+    return NGX_ERROR;
+  }
+
+  sentinel = ngx_slab_alloc(shpool, sizeof *sentinel);
+  if(sentinel == NULL) {
+    return NGX_ERROR;
+  }
+
+  ngx_rbtree_sentinel_init(sentinel);
+  tree->root = sentinel;
+  tree->sentinel = sentinel;
+  tree->insert = ngx_http_upstream_observed_rbtree_insert;
+  shm_zone->data = tree;
+  ngx_http_upstream_observed_rbtree = tree;
+
+  return NGX_OK;
+}
+
+static char *ngx_http_upstream_observed_set_shm_size(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+  ssize_t   new_shm_size;
+  ngx_str_t *value;
+
+  value = cf->args->elts;
+  
+  new_shm_size = ngx_parse_size(&value[1]);
+  if(new_shm_size == NGX_ERROR) {
+    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "Invalid memory area size '%V'", &value[1]);
+    return NGX_CONF_ERROR;
+  }
+
+  new_shm_size = ngx_align(new_shm_size, ngx_pagesize);
+
+  if(new_shm_size < 8 * (ssize_t) ngx_pagesize) {
+    ngx_conf_log_error(NGX_LOG_WARN, cf, 0, "The upstream_observed_shm_size value must be atleast %udKiB", (8 * ngx_pagesize) >> 10);
+  }
+
+  if(ngx_http_upstream_observed_shm_size && ngx_http_upstream_observed_shm_size != (ngx_uint_t) new_shm_size) {
+    ngx_conf_log_error(NGX_LOG_WARN, cf, 0, "Cannot change memory area size without restart, ignoringchange");
+  }else {
+    ngx_http_upstream_observed_shm_size = new_shm_size;
+  }
+
+  ngx_conf_log_error(NGX_LOG_DEBUG, cf, 0, "Using %udKiB of shared memory for upstream_observed", new_shm_size >> 10);
+
+  return NGX_CONF_OK;
+}
+
+static char *ngx_http_upstream_observed(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
+  ngx_http_upstream_srv_conf_t *uscf;
+  ngx_uint_t i;
+  ngx_uint_t extra_peer_flags = 0;
+
+  for(i = 1; i < cf->args->nelts; i++) {
+    ngx_str_t *value =  cf->args->elts;
+    if(ngx_strcmp(value[i].data, "no_rr") == 0) {
+      extra_peer_flags |= NGX_HTTP_UPSTREAM_OBSERVED_NO_RR;
+    }else if(ngx_strcmp(value[i].data, "weight_mode=peak") == 0) {
+      if(extra_peer_flags & NGX_HTTP_UPSTREAM_OBSERVED_WEIGHT_MODE_MASK) {
+	ngx_conf_log_error(NGX_CONF_EMERG, cf, 0, "weight_mode = options are mutually exclusive");
+	return NGX_CONF_ERROR;
+      }
+      extra_peer_flags |= NGX_HTTP_UPSTREAM_OBSERVED_WEIGHT_MODE_PEAK;
+    }else if(ngx_strcmp(value[i].data, "weight_mode=idle") == 0) {
+      ngx_conf_log_error(NGX_CONF_EMERG, cf, 0, "weight_mode = options are mutually exclusive");
+      return NGX_CONF_ERROR;
+    }
+    extra_peer_flags |= NGX_HTTP_UPSTREAM_OBSERVED_WEIGHT_MODE_IDLE;
+  }else {
+    ngx_conf_log_error(NGX_CONF_EMERG, cf, 0, "Invalid observed parameter '%V'", &value[1]);
+    return NGX_CONF_ERROR;
+  }
+
+  uscf = ngx_http_conf_get_module_srv_conf(cf, ngx_http_upstream_module);
+  
+  uscf->peer.init_upstream = ngx_http_upstream_init_observed;
+
+  uscf->flags = NGX_HTTP_UPSTREAM_CREATE | NGX_HTTP_UPSTREAM_WEIGHT | NGX_HTTP_UPSTREAM_MAX_FAILS | NGX_HTTP_UPSTREAM_FAIL_TIMEOUT | NGX_HTTP_UPSTREAM_DOWN | extra_peer_flags;
+
+  return NGX_CONF_OK;
+}
+
+static ngx_int_t ngx_http_upstream_cmp_servers(const void *one, const void *two) {
+  const ngx_http_upstream_observed_peer_t *first, *second;
+  
+  first = one;
+  second = two;
+
+  return (first->weight < second->weight);
+}
+
+static ngx_int_t ngx_http_upstream_init_observed(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t *us) {
+  ngx_url_t                          u;
+  ngx_uint_t                         i, j, n;
+  ngx_http_upstream_server_t         *server;
+  ngx_http_upstream_observed_peers_t *peers, *backup;
+
+  if(us->servers) {
+    server = us->servers->elts;
+
+    n = 0;
+
+    for(i = 0; i < us->servers->nelts; i++) {
+      if(server[i].backup) {
+	continue;
+      }
+
+      n += server[i].naddrs;
+    }
+
+    peers = ngx_palloc(cf->pool, sizeof(ngx_http_upstream_observed_peers_t) + sizeof(ngx_http_upstream_observed_peer_t) * (n - 1));
+
+    if(peers == NULL) {
+      return NGX_ERROR;
+    }
+
+    peers->number = n;
+    peers->name = &u->host;
+
+    n = 0;
+
+    for(i = 0; i < us->servers->nelts; i++) {
+      for(j = 0; j < server[i].naddrs; j++) {
+	if(server[i].backup) {
+	  continue;
+	}
+
+	peers->peer[n].sockaddr = server[i].addrs[j].sockaddr;
+	peers->peer[n].socklen = server[i].addrs[j].socklen;
+	peers->peer[n].name = server[i].addrs[j].name;
+	peers->peer[n].max_fails  = server[i].max_fails;
+	peers->peer[n].fail_timeout = server[i].fail_timeout;
+	peers->peer[n].down = server[i].down;
+	peers->peer[n].weight = server[i].down ? 0 : server[i].weight;
+	n++;
+      }
+    }
+
+    us->peer.data = peers;
+
+    ngx_sort(&peers->peer[0], (size_t) n, sizeof(ngx_http_upstream_observed_peer_t), ngx_http_upstream_cmp_servers);
+
+    n = 0;
+    
+    for(i = 0; i < us->servers->nelts; i++) {
+      if(!server[i].backup) {
+	continue;
+      }
+
+      n += server[i].naddrs;
+    }
+
+    if(n == 0) {
+      return NGX_OK;
+    }
+
+    backup = ngx_palloc(cf->pool, sizeof(ngx_http_upstream_observed_peers_t) + sizeof(ngx_http_upstream_observed_peer_t) * (n - 1));
+
+    if(backup == NULL) {
+      return NGX_ERROR;
+    }
+
+    backup->number = n;
+    backup->name = &us->host;
+
+    n = 0;
+
+    for(i = 0; i < us->servers->nelts; i++) {
+      for(j = 0; j < server[i].naddrs; j++) {
+	if(!server[i].backup) {
+	  continue;
+	}
+
+	backup->peer[n].sockaddr = server[i].addrs[j].sockaddr;
+	backup->peer[n].socklen = server[i].addrs[j].socklen;
+	backup->peer[n].name = server[i].addrs[j].name;
+	backup->peer[n].weight = server[i].weight;
+	backup->peer[n].max_fails = server[i].max_fails;
+	backup->peer[n].fail_timeout = server[i].fail_timeout;
+	backup->peer[n].down = server[i].down;
+	n++;
+      }
+    }
+
+    peers->next = backup;
+
+    ngx_sort(&backup->peer[0], (size_t) n, sizeof(ngx_http_upstream_observed_peer_t), ngx_http_upstream_cmp_servers);
+
+    return NGX_OK;
+  }
+
+  if(us->port == 0 && us->default_port == 0) {
+    ngx_log_error(NGX_LOG_EMERG, cf->log, 0, "no port in upstream \"%V\" in %s:%ui", &us->host, us->file_name, us_line);
+    return NGX_ERROR;
+  }
+
+  ngx_memzero(&u, sizeof(ngx_url_t));
+
+  u.host = us->host;
+  u.port = (in_port_t) (us->port ? us->port : us->default_port);
+
+  if(ngx_inet_resolve_host(cf_pool, &u) != NGX_OK) {
+    if(u.err) {
+      ngx_log_error(NGX_LOG_EMERG, cf->log, 0, "%s in upstream \"%V\" in %s:%ui", u.err, &us->host, us->file_name, us->line);
+    }
+
+    return NGX_ERROR;
+  }
+
+  n = u.addrs;
+
+  peers = ngx_palloc(cf->pool, sizeof(ngx_http_upstream_observed_peer_t) + sizeof(ngx_http_upstream_observed_peer_t) * (n - 1));
+  if(peers == NULL) {
+    return NGX_ERROR;
+  }
+
+  peers->number = n;
+  peers->name = &us->host;
+
+  for(i = 0; i < u.naddrs; i++) {
+    peers->peer[i].sockaddr = u.addrs[i].sockaddr;
+    peers->peer[i].socklen = u.addrs[i].socklen;
+    peers->peer[i].name = u.addrs[i].name;
+    peers->peer[i].weight = 1;
+    peers->peer[i].max_fails = 1;
+    peers->peer[i].fail_timeout = 10;
+  }
+
+  us->peer.data = peers;
+
+  return NGX_OK;
+}
